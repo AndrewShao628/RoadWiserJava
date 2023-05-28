76cdfef7345c6430b968e07af2f83ca47d7e4889
AndrewShao628 <shaojinghong@gmail.com>
2023-05-28 10:05:18 -0400

    Update SplashActivity.java

--- app/src/main/java/com/example/roadwiserjava/SplashActivity.java
+++ app/src/main/java/com/example/roadwiserjava/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.roadwiserjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -16,8 +17,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         }, 3000);
     }
