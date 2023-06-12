b148e5c79ec0d3e7f3a6f84f9473d01452ab383d
AndrewShao628 <shaojinghong@gmail.com>
2023-06-11 23:36:15 -0400

    Added CameraX functionality

--- app/src/main/java/com/example/roadwiserjava/MainActivity.java
+++ app/src/main/java/com/example/roadwiserjava/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.roadwiserjava;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Camera;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -11,31 +24,28 @@ import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+//select camera, start recording, every two minutes make clip detect motion
 
 public class MainActivity extends AppCompatActivity {
-    private Camera mCamera;
-    private MediaRecorder recorder;
-    private SurfaceHolder holder;
-
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private boolean recording = false;
+    private PreviewView previewView;
+    private VideoCapture videoCapture;
     private ImageButton settingsButton;
     private ImageButton loadScreenButton;
+    private ImageButton photoButton, recordButton;
     private static final String TAG = "Sigwise";
     private volatile int recstate = 0;
 
-
-    @Override
-    protected void finalize() throws Throwable {
-        try{
-            if(mCamera != null){
-                mCamera.release();
-                mCamera = null;
-            }
-        }
-        finally {
-            super.finalize();
-        }
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +60,23 @@ public class MainActivity extends AppCompatActivity {
         settingsButton = (ImageButton) findViewById(R.id.settingsButton);
         loadScreenButton = (ImageButton) findViewById(R.id.loadScreenButton);
 
+        //photoButton = (ImageButton) findViewById(R.id.photoButton);
+        recordButton = (ImageButton) findViewById(R.id.recordButton);
+
+        previewView = (PreviewView) findViewById(R.id.previewView);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, getExecutor());
+
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,109 +90,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final ImageButton button_rec = (ImageButton) findViewById(buttonRec);
-        recorder = new MediaRecorder();
-        button_rec.setOnClickListener(new View.OnClickListener() {
+        recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (recstate == 0) {
-                    mCamera.stopPreview();
-                    startRecord(); //recstate is set inside start and stop record
-                    button_rec.setImageDrawable(getResources().getDrawable(R.drawable.stoprec2));
-                    settingsButton.setVisibility(View.INVISIBLE);
-                    loadScreenButton.setVisibility(View.INVISIBLE);
-                    EditText tvSpeed = (EditText)findViewById(R.id.editTextSpeed);
-                    tvSpeed.setVisibility(View.VISIBLE);
-                } else {
-                    stopRecord();
-                    mCamera.startPreview();
-                    bProtected = false;
-                    button_rec.setImageDrawable(getResources().getDrawable(R.drawable.record4));
-                    button_config.setVisibility(View.VISIBLE);
-                    button_load.setVisibility(View.VISIBLE);
-                    EditText tvSpeed = (EditText)findViewById(R.id.editTextSpeed);
-                    tvSpeed.setVisibility(View.INVISIBLE);
-                }
-
-            }
-
-        });
-    }
-
-    private void initRecorder(){
-        recorder.reset();
-        mCamera.unlock();
-        recorder.setCamera(mCamera);
-        SigwiseLogger.i(TAG, "reset Recorder");
-        try{
-            recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
-            SigwiseLogger.i(TAG, "setAudioSource");
-            recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-            SigwiseLogger.i(TAG, "setVideoSource");}
-        catch (Exception e){
-            SigwiseLogger.i(TAG,e.toString());
-            e.printStackTrace();
-            finish();
-        }
-
-        CamcorderProfile cp;
-        if(resostate == 1) {
-            cp = CamcorderProfile
-                    .get(CamcorderProfile.QUALITY_LOW);
-        }
-        else if(resostate == 3) {
-            cp = CamcorderProfile
-                    .get(CamcorderProfile.QUALITY_HIGH);
-        }
-        else{
-            int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-            if (currentapiVersion >= Build.VERSION_CODES.HONEYCOMB) {
-                if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_720P)) {
-                    cp = CamcorderProfile
-                            .get(CamcorderProfile.QUALITY_720P);
-                } else if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_480P)) {
-                    cp = CamcorderProfile
-                            .get(CamcorderProfile.QUALITY_480P);
-                } else {
-                    cp = CamcorderProfile
-                            .get(CamcorderProfile.QUALITY_LOW);
-                }
-            }
-            else{
-                cp = CamcorderProfile
-                        .get(CamcorderProfile.QUALITY_LOW);
-            }
-        }
-        recorder.setProfile(cp);
-        SigwiseLogger.i(TAG, "setProfile");
-        path = getOutputMediaFile().toString();
-        SigwiseLogger.i(TAG,path);
-        recorder.setOutputFile(path.substring(8));
-        SigwiseLogger.i(TAG, "setOutputFile");
-        recorder.setMaxDuration(videocliplen*60*1000); // videocliplen*60 seconds
-        SigwiseLogger.i(TAG, "setMaxDuration");
-        recorder.setMaxFileSize(1000000000); // Approximately 1000 megabytes per file
-        SigwiseLogger.i(TAG, "setMaxFileSize");
-        recorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
-            @Override
-            public void onInfo(MediaRecorder mr, int what, int extra) {
-                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
-                    stopRecord();
-                    startRecord();
-                }
+                record();
             }
         });
-        try {
-            recorder.prepare();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            finish();
-        } catch (IOException e) {
-            e.printStackTrace();
-            finish();
-        }
-        SigwiseLogger.i(TAG,"Recorder prepare");
+
     }
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll(); //unbinds camera
+
+        //Camera Selector use case
+        CameraSelector cameraSelector = new CameraSelector.Builder()    //selects the default camera
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        //Preview use case
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        //Image Capture use case
+
+        //Video Capture use case
+        videoCapture = new VideoCapture.Builder().setVideoFrameRate(30).build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, videoCapture);
+    }
+
 
     public void openSettings() {
         Intent intent = new Intent(this, SettingsActivity.class);
@@ -177,5 +136,57 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoadScreenActivity.class);
         startActivity(intent);
         SigwiseLogger.i(TAG, "open load screen");
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void record() {
+        if (!recording) {
+            recordButton.setImageResource(R.drawable.stoprec2);
+
+            if (videoCapture != null) {
+                File movieDir = new File("mnt/sdcard/Movies/CameraXMovies");    //might be incorrect
+                if (!movieDir.exists()) {
+                    movieDir.mkdir();
+                }
+                Date date = new Date();
+                String timestamp = String.valueOf(date.getTime());
+                String vidFilePath = movieDir.getAbsolutePath() + "/" + timestamp + ".jpg";
+
+                File vidFile = new File(vidFilePath);
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                videoCapture.startRecording(
+                        new VideoCapture.OutputFileOptions.Builder(vidFile).build(),
+                        getExecutor(),
+                        new VideoCapture.OnVideoSavedCallback() {
+                            @Override
+                            public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+                                Toast.makeText(MainActivity.this, "Video has been saved successfully.", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                                Toast.makeText(MainActivity.this, "Error saving the video: " + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+            }
+
+            recording = true;
+        }
+        else {
+            recordButton.setImageResource(R.drawable.record4);
+            videoCapture.stopRecording();
+            recording = false;
+        }
     }
 }
