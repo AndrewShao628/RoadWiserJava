cb08d4308d8c81b2352d5af45da50adf66914e3f
AndrewShao628 <shaojinghong@gmail.com>
2023-06-29 23:27:17 -0400

    Integrated OpenCV

---
+++ OpenCV/build/intermediates/cxx/Debug/432g6r30/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\blue_\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\blue_\\AndroidStudioProjects\\RoadWiserJava\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\blue_\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\blue_\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\blue_\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\blue_\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\blue_\\AndroidStudioProjects\\RoadWiserJava\\OpenCV\\build\\intermediates\\cxx\\Debug\\432g6r30\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\blue_\\AndroidStudioProjects\\RoadWiserJava\\OpenCV\\build\\intermediates\\cxx\\Debug\\432g6r30\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\blue_\\AndroidStudioProjects\\RoadWiserJava\\OpenCV\\.cxx\\Debug\\432g6r30\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
